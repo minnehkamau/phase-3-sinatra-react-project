51e487230a4ad977c052aeb1c63b6e824cdb8b28
minnehkamau <minnieches08@gmail.com>
2023-02-28 20:44:09 +0300

    reviews migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_28_173146) do
+ActiveRecord::Schema.define(version: 2023_02_28_173715) do
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "comment"
+    t.integer "rating"
+    t.string "user_id"
+    t.string "book_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
