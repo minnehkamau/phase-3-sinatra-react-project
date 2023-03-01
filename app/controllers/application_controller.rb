8a8fb0382238fa6d6404dab05e2a16fa23e97319
minnehkamau <minnieches08@gmail.com>
2023-03-01 17:00:13 +0300

    single book route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,21 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
+  #getting all books
   get "/" do
+    { message: "Getting started with my project" }.to_json
+  end
+
+  get "/books" do
     books = Book.all
     p books
     books.to_json()
+  end
+
+  #geting a single book
+  get "/books/:id" do
+    book = Book.find_by(id: params[:id])
+    book.to_json()
   end
 
 end
